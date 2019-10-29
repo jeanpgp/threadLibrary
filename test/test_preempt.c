c6a35c15ab01ba2eb111821acac5cd421ed58e13
Nikhil Razdan <39114568+nsrazdan@users.noreply.github.com>
2019-10-28 17:32:44 -0700

    Update test_preempt.c

--- test/test_preempt.c
+++ test/test_preempt.c
@@ -5,7 +5,7 @@
 
 long int MAX = 10000;
 
-int huge(void)
+int huge(void* arg)
 {	
 	printf("Running huge loop\n");
 	for (int i = 0; i < MAX; i++) {
@@ -14,13 +14,15 @@ int huge(void)
 
 	printf("thread%d", uthread_self());
 
-	return 0;
+	return 3;
 }
 
-int hello_world(void) {
+int hello_world(void* arg) {
 	for (int i = 0; i < 10; i++) {
 		printf("Hello world!");
 	}
+	
+	return 6;
 }
 
 int main(void)
